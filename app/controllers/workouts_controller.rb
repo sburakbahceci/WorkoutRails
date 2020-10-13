fdd7c1a6feb2bad09cab2b1b9fcb5997ee41ee25
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-13 14:31:50 +0300

    Add App Gems & Workout Forms / Views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,38 @@
 class WorkoutsController < ApplicationController
+    before_action :find_workout, only: [:show, :edit, :update, :destroy]
     def index
         
     end
-    
+    def show
+        
+    end
+    def new
+        @workout = Workout.new
+    end
+    def create
+        @workout = Workout.new(workouts_params)
+        if @workout.save
+            redirect_to @workout
+        else
+            render 'new'
+        end
+    end
+    def edit
+        
+    end
+    def update
+        
+    end
+    def destroy
+        
+    end
+
+    private
+
+    def workouts_params
+        params.require(:workout).permit(:date, :workout, :mood, :length)
+    end
+    def find_workout
+        @workout = Workout.find(params[:id])
+    end
 end
